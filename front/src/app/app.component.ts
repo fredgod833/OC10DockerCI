ef73d2ba42212db56b449769fdd0f8418962bd3c
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2024-04-19 10:54:57 +0200

    Add an big error

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Joke } from './model/joke.model';
 import { JokesService } from './services/jokes.service';
@@ -6,20 +6,18 @@ import { JokesService } from './services/jokes.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   public joke$: Observable<Joke | null> = this.jokesService.joke$();
 
-  constructor(private jokesService: JokesService) {
-  }
+  constructor(private jokesService: JokesService) {}
 
   public ngOnInit(): void {
     this.getRandomJoke();
   }
 
-  public getRandomJoke(): void {
-    this.jokesService.getRandomJoke();
-  }
+  // public getRandomJoke(): void {
+  //   this.jokesService.getRandomJoke();
+  // }
 }
