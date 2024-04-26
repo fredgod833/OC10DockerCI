7708608515f65dff7814c6fe842a90d0537f1af5
citizensyd <59608029+citizensyd@users.noreply.github.com>
2024-04-26 15:25:12 +0200

    Test github action

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,12 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {JokesService} from "./services/jokes.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [HttpClientTestingModule],
+      schemas: [NO_ERRORS_SCHEMA],
       providers: [JokesService]
     }).compileComponents();
   });
