ec0102eab1071a46cd7a0eb582a065d8c12b58f5
Younes Lahouiti <loller763@gmail.com>
2024-05-01 11:28:10 +0200

    Removal of the bug added on purpose

--- back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
+++ back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
@@ -3,7 +3,7 @@ package com.openclassrooms.bobapp.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.bobapp.service.JokeService;
 
