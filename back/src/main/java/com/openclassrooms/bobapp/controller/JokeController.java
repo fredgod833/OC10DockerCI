d1df2310ff7d364e6db4e4200129420ebfcfa8e8
Audrey J Silva <dejesusdasilva.marvin@gmail.com>
2024-05-05 10:19:34 +0200

    Test sonarqube

--- back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
+++ back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
@@ -3,6 +3,8 @@ package com.openclassrooms.bobapp.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.bobapp.service.JokeService;
@@ -20,5 +22,15 @@ public class JokeController {
     @GetMapping()
     public ResponseEntity<?> getRandomJokes() {
         return ResponseEntity.ok(this.jokeService.getRandomJoke());
+    }
+
+     @GetMapping("/github/callback")
+    @ResponseBody
+    public String handleGithubCallback(@RequestParam("code") String code) {
+        // Handle the callback logic here
+        // Extract code and exchange it for access token
+        // Store the access token securely
+        
+        return "Callback handled successfully" + code;
     }
 }
