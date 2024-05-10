d5ec2da1515d6135b7561ed3dccfcc49443cfd1a
Stephanefy <stephanefokyin@gmail.com>
2024-05-10 10:17:28 +0200

    Actions workflow: changed actions version, added a test on the frontend, added a step in angular workflow to save test coverage report as an artifact

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,8 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {JokesService} from "./services/jokes.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let jokesService: JokesService;
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
@@ -12,9 +18,23 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  beforeEach(() => {
+    jokesService = TestBed.inject(JokesService);
+    spyOn(jokesService, 'getRandomJoke');
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  })
+
+  it('should create the app', () => {
+ 
+    expect(component).toBeTruthy();
+  });
+
+  it('should get a random joke', () => {
+    component.getRandomJoke();
+    expect(jokesService.getRandomJoke).toHaveBeenCalled();
+    expect(component.joke$).toBeDefined();
+   
   });
 });
