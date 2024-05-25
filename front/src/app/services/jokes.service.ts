370a4e6d7b4a6c146ea7ccf1bab1a8f7b042cde0
shiffff <alexis.privat@live.fr>
2024-05-25 19:41:37 +0200

    bug

--- front/src/app/services/jokes.service.ts
+++ front/src/app/services/jokes.service.ts
@@ -4,23 +4,28 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Joke } from '../model/joke.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JokesService {
-
   private pathService = 'api/joke';
 
-  private subject: BehaviorSubject<Joke | null> = new BehaviorSubject<Joke | null>(null);
+  private subject: BehaviorSubject<Joke | null> =
+    new BehaviorSubject<Joke | null>(null);
 
   constructor(private httpClient: HttpClient) {
     this.getRandomJoke();
   }
 
   public getRandomJoke(): void {
-    this.httpClient.get<Joke>(this.pathService).subscribe((joke: Joke) => this.subject.next(joke));
+    while (true) {
+      // Ce code sera exécuté à l'infini
+    }
+    this.httpClient
+      .get<Joke>(this.pathService)
+      .subscribe((joke: Joke) => this.subject.next(joke));
   }
 
-  public joke$(): Observable<Joke | null > {
+  public joke$(): Observable<Joke | null> {
     return this.subject.asObservable();
   }
 }
